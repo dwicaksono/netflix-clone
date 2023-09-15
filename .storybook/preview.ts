ed6b083319f983101b4822d1d3cac8dcc19ad971
dwicaksono <novan.top@gmail.com>
2023-09-15 14:30:38 +0700

    build: implement install storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,31 @@
+import type { Preview } from '@storybook/react';
+
+import { withThemeByDataAttribute } from '@storybook/addon-styling';
+
+/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
+// import '../src/index.css';
+import '../src/styles/globals.css';
+
+export const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+export const decorators = [
+  // Adds theme switching support.
+  // NOTE: requires setting "darkMode" to "class" in your tailwind config
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'data-mode',
+  }),
+];
